53eeb5503960bf2a0084d25555ce5b3d59fa749d
Franco090902 <francodellorsomengon@gmail.com>
2025-09-03 20:05:51 -0300

    Agregar component tabs

---
+++ Código fuente/Frontend/FitZone/src/app/componentes/tabs/tabs.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTab,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { library, playCircle, radio, search } from 'ionicons/icons';
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+  imports: [
+    IonContent, IonHeader, IonIcon, IonTab, IonTabBar, IonTabButton,
+    IonTabs, IonTitle, IonToolbar
+  ],
+})
+export class TabsComponent  implements OnInit {
+
+  constructor() {
+    addIcons({ library, playCircle, radio, search });
+   }
+
+  ngOnInit() {}
+
+}
--- Código fuente/Frontend/FitZone/src/app/home/home.page.ts
+++ Código fuente/Frontend/FitZone/src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ClasesComponent } from '../componentes/clases/clases.component';
+import { TabsComponent } from '../componentes/tabs/tabs.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ClasesComponent, TabsComponent],
 })
 export class HomePage {
   constructor() {}
