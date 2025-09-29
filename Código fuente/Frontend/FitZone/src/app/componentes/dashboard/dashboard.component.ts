f9c6724d4cba3e49381c1a239ef055e82ccd503d
Franco090902 <francodellorsomengon@gmail.com>
2025-09-29 20:52:41 -0300

    modificacion componente dashboard imports

--- Código fuente/Frontend/FitZone/src/app/componentes/dashboard/dashboard.component.ts
+++ Código fuente/Frontend/FitZone/src/app/componentes/dashboard/dashboard.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    RouterModule
+  ]
 })
 export class DashboardComponent  implements OnInit {
 
