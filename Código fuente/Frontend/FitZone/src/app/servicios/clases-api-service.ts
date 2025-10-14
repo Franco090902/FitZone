60f0ab358fa98153c0c5d45503871e55214603c9
Franco090902 <francodellorsomengon@gmail.com>
2025-10-14 15:31:54 -0300

    cambios apis clases

--- Código fuente/Frontend/FitZone/src/app/servicios/clases-api-service.ts
+++ Código fuente/Frontend/FitZone/src/app/servicios/clases-api-service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface Clase {
+  id_clase: number;
+  nombre: string;
+  descripcion: string;
+  capacidad: number;
+  fecha_hora: string;
+  id_gimnasio: number;
+  cupos_disponibles: number;
+}
+
+@Injectable({ providedIn: 'root' })
 export class ClasesApiService {
-  
+  private api = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+  getClases(idGimnasio?: number): Observable<Clase[]> {
+    const url = idGimnasio ? `${this.api}/clases?gimnasio=${idGimnasio}` : `${this.api}/clases`;
+    return this.http.get<Clase[]>(url);
+  }
 }
--- Código fuente/Frontend/FitZone/src/app/servicios/trainers-api-service.ts
+++ Código fuente/Frontend/FitZone/src/app/servicios/trainers-api-service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+export interface Trainer {
+  id_trainer: number;
+  nombre: string;
+  especialidad: string;
+  contacto: string;
+  id_gimnasio: number;
+}
+
+@Injectable({ providedIn: 'root' })
 export class TrainersApiService {
-  
+  private api = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+  getTrainers(): Observable<Trainer[]> {
+    return this.http.get<Trainer[]>(`${this.api}/trainers`);
+  }
 }
