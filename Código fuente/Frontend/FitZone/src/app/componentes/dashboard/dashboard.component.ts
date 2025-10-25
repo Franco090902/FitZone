39cb42c382f549d8e7a032445902b129cc05cdf5
mattiiasbernar2006-ship-it <mattiiasbernar2006@gmail.com>
2025-10-25 15:57:01 -0300

    dashboard

--- Código fuente/Frontend/FitZone/src/app/componentes/dashboard/dashboard.component.ts
+++ Código fuente/Frontend/FitZone/src/app/componentes/dashboard/dashboard.component.ts
@@ -1,26 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { RouterLink } from '@angular/router';
+import { DashboardService } from '../../servicios/dashboard.service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
+    CommonModule,
+    IonicModule,
     RouterLink
   ]
 })
-export class DashboardComponent  implements OnInit {
+export class DashboardComponent implements OnInit {
+  reservas: any[] = [];
+  membresia: any = null;
+  clasesDisponibles: any[] = [];
+  reservasEspacios: any[] = [];
+  clienteInfo: any = null;
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Obtener el ID del cliente del localStorage
+    const idCliente = Number(localStorage.getItem('idCliente'));
+    if (idCliente) {
+      this.cargarDatos(idCliente);
+    }
+  }
 
+  cargarDatos(idCliente: number) {
+    // Cargar información del cliente
+    this.dashboardService.getClienteInfo(idCliente)
+      .subscribe(data => this.clienteInfo = data);
+
+    // Cargar reservas del cliente
+    this.dashboardService.getReservasCliente(idCliente)
+      .subscribe(data => this.reservas = data);
+
+    // Cargar membresía
+    this.dashboardService.getMembresia(idCliente)
+      .subscribe(data => this.membresia = data);
+
+    // Cargar clases disponibles
+    this.dashboardService.getClasesDisponibles()
+      .subscribe(data => this.clasesDisponibles = data);
+
+    // Cargar reservas de espacios
+    this.dashboardService.getReservasEspacios(idCliente)
+      .subscribe(data => this.reservasEspacios = data);
+  }
 }
